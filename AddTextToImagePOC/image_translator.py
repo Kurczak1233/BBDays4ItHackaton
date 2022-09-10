0a47f10d8d031681ae6eaf4618004b53ad43c692
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2022-09-10 23:01:22 +0200

    refactor

--- AddTextToImagePOC/image_translator.py
+++ AddTextToImagePOC/image_translator.py
@@ -2,18 +2,18 @@ from PIL import Image, ImageFont
 from PIL import ImageDraw
 
 
-def get_font(txt: str, max_width: int):
-    font_size = 1
+def translate_text_on_image(image, text_line):
+    left = text_line["Geometry"]["BoundingBox"]["Left"]
+    top = text_line["Geometry"]["BoundingBox"]["Top"]
+    max_width = text_line["Geometry"]["BoundingBox"]["Width"]
+    max_height = text_line["Geometry"]["BoundingBox"]["Height"]
 
-    font_name = "segoeui.ttf"
+    text = text_line["DetectedText"]
+    text_color = "blue"
 
-    font = ImageFont.truetype(font_name, font_size)
+    img = draw_text_on_image(image, text, left, top, text_color, max_width, max_height)
 
-    while font.getsize(txt)[0] < max_width:
-        font_size += 1
-        font = ImageFont.truetype(font_name, font_size)
-
-    return font
+    return img
 
 
 def draw_text_on_image(img: Image,
@@ -28,7 +28,6 @@ def draw_text_on_image(img: Image,
 
     width, height = img.size
     max_width = width * max_width_percent
-    max_height = height * max_height_percent
 
     left_corner_x = left_corner_x_percent * width
     left_corner_y = left_corner_y_percent * height
@@ -40,12 +39,18 @@ def draw_text_on_image(img: Image,
     draw.rectangle((left - 5, top - 5, right + 5, bottom + 5), fill="white")
     draw.text(coords, text, font=font, fill=text_color)
 
-    # draw.line(
-    #     [
-    #         (left_corner_x, left_corner_y),
-    #         (left_corner_x_percent + max_width, left_corner_y)
-    #     ],
-    #     fill="black",
-    #     width=3)
-
     return img
+
+
+def get_font(txt: str, max_width: int):
+    font_size = 1
+
+    font_name = "segoeui.ttf"
+
+    font = ImageFont.truetype(font_name, font_size)
+
+    while font.getsize(txt)[0] < max_width:
+        font_size += 1
+        font = ImageFont.truetype(font_name, font_size)
+
+    return font
--- AddTextToImagePOC/main.py
+++ AddTextToImagePOC/main.py
@@ -1,42 +1,17 @@
 from PIL import Image
-from types import SimpleNamespace
 
-from image_drawer import draw_text_on_image
+from image_translator import translate_text_on_image
 from ocr_response_handler import get_ocr_response, filter_only_lines_in_response
 
 response = get_ocr_response()
 filtered_response = filter_only_lines_in_response(response)
 
-
-def draw_on_img(image, text_line):
-    left = text_line["Geometry"]["BoundingBox"]["Left"]
-    top = text_line["Geometry"]["BoundingBox"]["Top"]
-    max_width = text_line["Geometry"]["BoundingBox"]["Width"]
-    max_height = text_line["Geometry"]["BoundingBox"]["Height"]
-
-    text = text_line["DetectedText"]
-    text_color = "blue"
-
-    img = draw_text_on_image(image, text, left, top, text_color, max_width, max_height)
-
-    return img
-
-
 file_test_name = "test.jpg"
 image = Image.open(f'img/{file_test_name}')
 
-img1 = image
+translated_image = image
 
 for x in filtered_response:
-    img1 = draw_on_img(img1, x)
+    translated_image = translate_text_on_image(translated_image, x)
 
-img1.save(f'output/{file_test_name}')
-
-# file_test2_name = "test2.png"
-# image2 = Image.open(f'img/{file_test2_name}')
-# text_coordinates2 = (50, 60)
-# text2 = "hejka2"
-# text_color2 = (255, 0, 255)
-# img2 = draw_text_on_image(image2, text2, text_coordinates2, text_color2)
-# img2.save(f'output/{file_test2_name}')
-#
+translated_image.save(f'output/{file_test_name}')
