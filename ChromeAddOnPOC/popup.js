fdf36f3bc203725666b22cfab0c0c563f8cb0361
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2022-09-11 10:04:51 +0200

    refactoring

--- ChromeAddOnPOC/popup.js
+++ ChromeAddOnPOC/popup.js
@@ -1,29 +1,16 @@
 // Initialize button with user's preferred color
-let redirectBtn = document.getElementById("redirect");
+let translateBtn = document.getElementById("translate");
 
-redirectBtn.addEventListener("click", async () => {
+translateBtn.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    func: redirect,
+    func: translateImages,
   });
 });
 
-function redirect() {
-  //   TODO: We need to run this code after tab reload..
-  //   var imgs = document.getElementsByTagName("img");
-  //   var imgSrcs = [];
-  //   for (var i = 0; i < imgs.length; i++) {
-  //     imgSrcs.push(imgs[i].src);
-  //   }
-  //   console.log(imgs);
-
-  //   const currentUrl = window.location.href;
-  //   const targetLanguage = "pl";
-  //   const url = `http://translate.google.com/translate?hl=${targetLanguage}&ie=UTF-8&u=${currentUrl}&sl=de&tl=${targetLanguage}`;
-  //   location.replace(url);
-
+function translateImages() {
   let images = document.getElementsByTagName("img");
   console.log(images);
 
@@ -44,18 +31,4 @@ function redirect() {
       }
     );
   }
-
-  //   let images = document.getElementsByTagName("img");
-  //   console.log(images);
-  //   for (let i = 0; i < images.length; i++) {
-  //     console.log("Sent");
-  //     console.log(object);
-  //     chrome.runtime.sendMessage(
-  //       { msg: "image", index: i, link: images[index].src },
-  //       function ({ data, index }) {
-  //         console.log("hej");
-  //         images[index].src = `data:image/jpeg;base64, ${data}`;
-  //       }
-  //     );
-  //   }
 }
